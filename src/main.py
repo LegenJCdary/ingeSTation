3e2c3f40756d83a38555b363b5ff95eb2531f2a5
LegenJCdary <jay_cob@interia.pl>
2023-09-21 06:42:29 +0200

    Create CliInput class in misc.arguments module for basic args handling
    Use cli_options as main() argument and print them out.

--- src/main.py
+++ src/main.py
@@ -1,17 +1,20 @@
+from ingestation.modules.misc.arguments import CliInput
 from ingestation.modules.misc.utils import InitParams
 
 
-def main() -> None:
+def main(cli_options: dict) -> None:
     init_params = InitParams()
     init_attrs = \
         [attr for attr in dir(init_params) if not any(attr.startswith(x) for x in ["_", "get"])]
     print_params = ", ".join([str(getattr(init_params, attr)) for attr in init_attrs])
+    print_options = ", ".join([f"{k}={v}" for k, v in cli_options.items()])
     print("Welcome to ingeSTation project!")
     print(f"Started with: {print_params}")
+    print(f"Supplied options: {print_options}")
 
 
 def ingestation():
-    return main()
+    return main(CliInput.parse_arguments(CliInput()))
 
 
 if __name__ == "__main__":
