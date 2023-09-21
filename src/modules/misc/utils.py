b5e7a97aeeba920be7e253e433e2df5beeb67a03
LegenJCdary <jay_cob@interia.pl>
2023-09-21 05:34:29 +0200

    Create InitParams class in misc module and print attributes in main
    Remove print_something() and replace usage with print().

---
+++ src/modules/misc/utils.py
@@ -0,0 +1,22 @@
+from datetime import datetime
+from os import getlogin
+from pwd import getpwnam
+from socket import gethostname
+from typing import Tuple
+
+
+class InitParams:
+
+    def __init__(self):
+        self.start_point = self.get_start_point()
+        self.caller_uid, self.caller_gid, self.caller_name = self.get_caller_id()
+        self.hostname = gethostname()
+
+    @staticmethod
+    def get_start_point() -> str:
+        return datetime.now().strftime("%Y%m%d_%H%M%S")
+
+    @staticmethod
+    def get_caller_id() -> Tuple[int, int, str]:
+        user = getpwnam(getlogin())
+        return user.pw_uid, user.pw_gid, user.pw_name
