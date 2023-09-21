d65e010169dc5329d5a23db7f21a70dfa7a8fd02
LegenJCdary <jay_cob@interia.pl>
2023-09-21 05:34:29 +0200

    Create InitParams class in misc module and print attributes in main
    Remove print_something() and replace usage with print().

--- src/main.py
+++ src/main.py
@@ -1,12 +1,13 @@
-from typing import Optional
+from ingestation.modules.misc.utils import InitParams
 
 
-def print_something(something: Optional[str]) -> None:
-    print(something)
-
-
-def main():
-    print_something("Welcome to ingeSTation project!")
+def main() -> None:
+    init_params = InitParams()
+    init_attrs = \
+        [attr for attr in dir(init_params) if not any(attr.startswith(x) for x in ["_", "get"])]
+    print_params = ", ".join([str(getattr(init_params, attr)) for attr in init_attrs])
+    print("Welcome to ingeSTation project!")
+    print(f"Started with: {print_params}")
 
 
 def ingestation():
