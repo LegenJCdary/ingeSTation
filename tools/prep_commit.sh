f04865918905f94d2c61029b4046e38d406a2f44
LegenJCdary <jay_cob@interia.pl>
2023-11-07 10:04:36 +0100

    Create developer tools - prep_commit.sh and prep_test.sh
    They are useful until git-hooks are enabled in this project: prep_test enables
    running pylint and pytest directly on source code as well as code execution,
    prep_commit reverts those changes.

---
+++ tools/prep_commit.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+
+# Revert steps performed in prep_test.sh - remove sys.path extension from test scripts and add
+# application name to import statements.
+
+while read -r file; do
+	for x in {1..5}; do
+		sed -i '1d' "$file"
+	done
+done < <(git ls-files tests/**/*.py)
+
+while read -r file; do
+	sed -i 's/modules/ingestation\.modules/g' "$file"
+done < <(git ls-files '*.py')
---
+++ tools/prep_test.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+
+# Extend sys.path with ./src path - enables running pytest directly on source code
+while read -r file; do
+	sed -i '1s/^/\n/' "$file"
+	sed -i '1s/^/sys.path.append(src_path)\n/' "$file"
+	sed -i '1s/^/src_path = os.path.join(os.path.abspath(__file__).rsplit(os.sep, 4)[0], "src")\n/' "$file"
+	sed -i '1s/^/import sys\n/' "$file"
+	sed -i '1s/^/import os\n/' "$file"
+done < <(git ls-files tests/**/*.py)
+
+# Remove application name from import statements - enables source code execution and running pylint
+# directly on source code
+while read -r file; do
+	sed -i 's/ingestation\.modules/modules/g' "$file"
+done < <(git ls-files '*.py')
