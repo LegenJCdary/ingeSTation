354e24ef6d70bcd70e20140dc760d1ebdbed8ceb
zh23jemu <zh23j_emu@126.com>
2023-11-14 13:49:00 +0800

    fix config.py

--- src/modules/configs/config.py
+++ src/modules/configs/config.py
@@ -1,21 +1,21 @@
+import os
 from logging import Logger
 from json import loads
 from typing import Union
-import os
-
 from jsonschema import validate
 
-from .schemas import application, project, operator
-from ..global_vars import global_vars
+import schemas
+from ..global_vars import APPLICATION_DIR, CONFIG_RULES, MANDATORY_KEYS, PRECEDENCE_RULES
 from .templates import templates
 
 
 class Conf:
     """Class for parsing configuration files"""
 
-    def __init__(self, conf_path: Union[bool, str], conf_type: str):
+    def __init__(self, conf_path: Union[bool, str], conf_type: str, schema_file: schemas):
         self.conf_type = conf_type
         self.conf_path = self.get_conf_path(conf_path)
+        self.schema_file = schema_file
 
     def parse_conf(self) -> Union[bool, dict]:
         conf = self.load_conf()
@@ -25,11 +25,11 @@ class Conf:
 
     def validate_conf(self, conf: dict) -> None:
         if self.conf_type == "application":
-            schema_file = application
+            schema_file = schemas.application
         elif self.conf_type == "project":
-            schema_file = project
+            schema_file = schemas.project
         else:
-            schema_file = operator
+            schema_file = schemas.operator
         return validate(instance=conf, schema=getattr(schema_file, self.conf_type))
 
     def load_conf(self) -> dict:
@@ -38,67 +38,57 @@ class Conf:
 
     def get_conf_path(self, conf_path: Union[bool, str]) -> str:
         if not conf_path:
-            conf_path = self.get_conf_here()
             if os.path.isfile(conf_path):
                 return conf_path
-            conf_path = os.path.join(global_vars.APPLICATION_DIR, f"{self.conf_type}.json")
+            conf_path = os.path.join(APPLICATION_DIR, f"{self.conf_type}.json")
             if os.path.isfile(conf_path):
                 return conf_path
             raise FileNotFoundError(f"[CRITICAL]: {self.conf_type.capitalize()}"
                                     " configuration could not be obtained.")
         return conf_path
 
-    def get_conf_here(self) -> str:
-        for file in os.listdir(os.getcwd()):
-            if file.endswith(f"{self.conf_type}.json"):
-                return os.path.join(os.getcwd(), file)
-
-        return ""
-
 
 class OperatorConf(Conf):
     """Class for operator configuration files"""
 
     def __init__(self, conf_path: Union[str, bool]):
-        super().__init__(conf_path, "operator")
+        super().__init__(conf_path, "operator", schemas.operator)
 
 
 class ProjectConf(Conf):
     """Class for project configuration files"""
 
     def __init__(self, conf_path: Union[str, bool]):
-        super().__init__(conf_path, "project")
+        super().__init__(conf_path, "project", schemas.project)
 
 
 class ApplicationConf(Conf):
     """Class for application configuration files"""
 
     def __init__(self, conf_path: Union[str, bool]):
-        super().__init__(conf_path, "application")
+        super().__init__(conf_path, "application", schemas.application)
 
 
-# pylint: disable=too-many-instance-attributes
 class MergedConf:
     """Class for merging configuration files"""
 
-    def __init__(self, logger: Logger, options: dict):
+    def __init__(self, logger: Logger):
         self.logger = logger
-        self.options = options
 
-        self.operator = OperatorConf(options["operator_conf"]).parse_conf()
-        self.project = ProjectConf(options["project_conf"]).parse_conf()
-        self.application = ApplicationConf(options["application_conf"]).parse_conf()
+        self.operator = OperatorConf(os.path.join(APPLICATION_DIR, "operator.json")).parse_conf()
+        self.project = ProjectConf(os.path.join(APPLICATION_DIR, "project.json")).parse_conf()
+        self.application = ApplicationConf(os.path.join(APPLICATION_DIR, "application.json")).parse_conf()
 
         self.operator_keys = self.flatten_dict(
-            self.operator, exclude_keys=global_vars.config_rules["exclude_flatten"])
+            self.operator, exclude_keys=CONFIG_RULES["exclude_flatten"])
         self.project_keys = self.flatten_dict(
-            self.project, exclude_keys=global_vars.config_rules["exclude_flatten"])
+            self.project, exclude_keys=CONFIG_RULES["exclude_flatten"])
         self.application_keys = self.flatten_dict(
-            self.application, exclude_keys=global_vars.config_rules["exclude_flatten"])
+            self.application, exclude_keys=CONFIG_RULES["exclude_flatten"])
 
         self.final = self.create_final_conf()
 
-    def flatten_dict(self, dct, parent_key='', sep='_', exclude_keys=None):
+    def flatten_dict(self, dct, parent_key="", sep="_", exclude_keys=None):
         if exclude_keys is None:
             exclude_keys = []
         flattened = {}
@@ -126,17 +116,17 @@ class MergedConf:
     def create_final_conf(self) -> dict:
         final_conf = {}
         for key in self.get_all_conf():
-            if key in global_vars.config_rules["exclude_flatten"]:
+            if key in CONFIG_RULES["exclude_flatten"]:
                 final_conf[key] = self.merge_conf(key)
             else:
                 final_conf[key] = self.overwrite_conf(key)
-            if not final_conf[key] and key in global_vars.mandatory_keys:
+            if not final_conf[key] and key in MANDATORY_KEYS:
                 # pylint: disable=broad-exception-raised
                 raise Exception(f"Missing mandatory value of: {key}")
         return final_conf
 
     def merge_conf(self, key: str):
-        if key in global_vars.config_rules["merge_to_dict"]:
+        if key in CONFIG_RULES["merge_to_dict"]:
             merge_conf = {}
             if key in self.application_keys:
                 merge_conf.update(self.application_keys[key])
@@ -156,31 +146,31 @@ class MergedConf:
 
     def overwrite_conf(self, key: str):
         # pylint: disable=too-many-return-statements, too-many-branches
-        if key in global_vars.precedence_rules["app_proj_op"]:
+        if key in PRECEDENCE_RULES["app_proj_op"]:
             if key in self.application_keys:
                 return self.application_keys[key]
             if key in self.project_keys:
                 return self.project_keys[key]
             return self.operator_keys[key]
-        if key in global_vars.precedence_rules["app_op_proj"]:
+        if key in PRECEDENCE_RULES["app_op_proj"]:
             if key in self.application_keys:
                 return self.application_keys[key]
             if key in self.operator_keys:
                 return self.operator_keys[key]
             return self.project_keys[key]
-        if key in global_vars.precedence_rules["proj_op_app"]:
+        if key in PRECEDENCE_RULES["proj_op_app"]:
             if key in self.project_keys:
                 return self.project_keys[key]
             if key in self.operator_keys:
                 return self.operator_keys[key]
             return self.application_keys[key]
-        if key in global_vars.precedence_rules["proj_app_op"]:
+        if key in PRECEDENCE_RULES["proj_app_op"]:
             if key in self.project_keys:
                 return self.project_keys[key]
             if key in self.application_keys:
                 return self.application_keys[key]
             return self.operator_keys[key]
-        if key in global_vars.precedence_rules["op_app_proj"]:
+        if key in PRECEDENCE_RULES["op_app_proj"]:
             if key in self.operator_keys:
                 return self.operator_keys[key]
             if key in self.application_keys:
