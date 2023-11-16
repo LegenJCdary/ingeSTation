d1080ab89aee6f9886dc1ec135b8a595bd65adbe
LegenJCdary <jay_cob@interia.pl>
2023-11-16 07:04:24 +0100

    More templates

---
+++ src/modules/configs/templates/project.py
@@ -0,0 +1,28 @@
+config = {
+    "destination": {
+        "root_path": None,
+        "endpoint": None,
+        "limits": {
+            "critical": None,
+            "warning": None
+        }
+    },
+    "permissions": {
+        "primary": {
+            "uid": None,
+            "gid": None,
+            "mode": None
+        },
+    },
+    "resources": {
+        "thread_count": None
+    },
+    "workdir": None,
+    "naming": {
+        "log_file": None,
+        "codename": None
+    },
+    "notify": None,
+    "exclude": None,
+    "include": None
+}
