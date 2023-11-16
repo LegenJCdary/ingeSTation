11eb4f4268cc81be618b3660566c79d45da83113
LegenJCdary <jay_cob@interia.pl>
2023-11-16 07:04:24 +0100

    More templates

---
+++ src/modules/configs/templates/application.py
@@ -0,0 +1,19 @@
+config = {
+    "containers": {
+        "sata": {
+            "device_path": None,
+            "mount_path": None
+        }
+    },
+    "resources": {
+        "thread_count": None
+    },
+    "workdir": None,
+    "naming": {
+        "log_file": None,
+        "codename": None
+    },
+    "notify": None,
+    "exclude": None,
+    "include": None
+}
