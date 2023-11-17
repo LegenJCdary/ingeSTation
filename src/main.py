e312666950ce685fff89b08ad1a733660c090d3f
LegenJCdary <jay_cob@interia.pl>
2023-11-17 10:23:45 +0100

    Update main

--- src/main.py
+++ src/main.py
@@ -1,7 +1,11 @@
 from ingestation.modules.misc.arguments import CliInput
-from ingestation.modules.misc.utils import InitParams
+from ingestation.modules.misc.utils import InitParams, Notify
 from ingestation.modules.outputs.loggers import Loggers
 from ingestation.modules.outputs import blocks
+from ingestation.modules.mounting.mounting import Mounting
+from ingestation.modules.partitioning.listing import DataPartiioning
+from ingestation.modules.threading.working import Workers
+from ingestation.modules.verification.verify import Verify
 
 
 def main(cli_options: dict) -> None:
@@ -10,6 +14,16 @@ def main(cli_options: dict) -> None:
     logger = loggers.logger
 
     blocks.log_starting_messages(logger, init_params, cli_options)
+
+    configuration = MergedConfig(logger).config
+    mela_plan = MOunting(configuration)
+    bites = DataPartiioning(logger, configuration, docking_list)
+
+    swallowed = Workers()
+    if swallowed:
+        verify = Verify()
+        if configuration.notify:
+            Notify().send_notification()
 
 
 def ingestation():
