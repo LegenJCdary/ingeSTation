12a3263dbfa48f32f8d03afdfb6d23f91246ffda
LegenJCdary <jay_cob@interia.pl>
2024-01-29 08:25:00 +0100

    Update prep tools to handle another nesting level in "tests" directory

--- tools/prep_commit.sh
+++ tools/prep_commit.sh
@@ -11,5 +11,11 @@ while read -r file; do
 done < <(git ls-files tests/**/*.py)
 
 while read -r file; do
+	for x in {1..5}; do
+		sed -i '1d' "$file"
+	done
+done < <(git ls-files tests/**/**/*.py)
+
+while read -r file; do
 	sed -i 's/modules/ingestation\.modules/g' "$file"
 done < <(git ls-files '*.py')
--- tools/prep_test.sh
+++ tools/prep_test.sh
@@ -10,6 +10,14 @@ while read -r file; do
 	sed -i '1s/^/import os\n/' "$file"
 done < <(git ls-files tests/**/*.py)
 
+while read -r file; do
+        sed -i '1s/^/\n/' "$file"
+        sed -i '1s/^/sys.path.append(src_path)\n/' "$file"
+        sed -i '1s/^/src_path = os.path.join(os.path.abspath(__file__).rsplit(os.sep, 4)[0], "src")\n/' "$file"
+        sed -i '1s/^/import sys\n/' "$file"
+        sed -i '1s/^/import os\n/' "$file"
+done < <(git ls-files tests/**/**/*.py)
+
 # Remove application name from import statements - enables source code execution and running pylint
 # directly on source code
 while read -r file; do
